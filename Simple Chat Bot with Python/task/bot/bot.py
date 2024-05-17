c643994cee2e2537cf3d65c8bff59c04c216cb4c
Malvi1697 <129746042+Malvi1697@users.noreply.github.com>
2024-05-17 20:52:36 +0200

    Refactored

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -1,58 +1,61 @@
-def greet(bot_name, birth_year):
-    print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
+def greet(bot_name: str, birth_year: str) -> None:
+    """Greets with the bot's name and birth year."""
+    print(f'Hello! My name is {bot_name}.')
+    print(f'I was created in {birth_year}.')
 
 
-def remind_name():
+def remind_name() -> None:
+    """Asks for user's name and compliments it."""
     print('Please, remind me your name.')
     name = input()
-    print('What a great name you have, ' + name + '!')
+    print(f'What a great name you have, {name}!')
 
 
-def guess_age():
+def guess_age() -> None:
+    """Guesses user's age based on remainders."""
     print('Let me guess your age.')
-    print('Enter remainders of dividing your age by 3, 5 and 7.')
-
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
+    print('Enter remainders of dividing your age by 3, 5, and 7.')
+    rem3 = int(input("Remainder when dividing by three: "))
+    rem5 = int(input("Remainder when dividing by five: "))
+    rem7 = int(input("Remainder when dividing by seven: "))
     age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    print(f"Your age is {age}; that's a good time to start programming!")
 
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
 
-
-def count():
+def count() -> None:
+    """Counts up to a number provided by the user."""
     print('Now I will prove to you that I can count to any number you want.')
-
     num = int(input())
-    curr = 0
-    while curr <= num:
-        print(curr, '!')
-        curr = curr + 1
+    for curr in range(num + 1):
+        print(f'{curr}!')
 
 
-def test():
+def test() -> None:
+    """Tests user's programming knowledge."""
     print("Let's test your programming knowledge.")
     print("Why do we use methods?")
     print("1. To repeat a statement multiple times.")
     print("2. To decompose a program into several small subroutines.")
     print("3. To determine the execution time of a program.")
     print("4. To interrupt the execution of a program.")
-    answer = input()
-    while answer != "4":
+    while (answer := input()) != "2":
         print("Please, try again.")
-        answer = input()
-    else:
-        end()
+    end()
 
 
-def end():
+def end() -> None:
+    """Ends the conversation."""
     print('Congratulations, have a nice day!')
 
 
-greet('Malvi', '2023')  # change it as you need
-remind_name()
-guess_age()
-count()
-test()
+def main() -> None:
+    """Main function to run all the tasks."""
+    greet('Malvi', '2024')
+    remind_name()
+    guess_age()
+    count()
+    test()
 
+
+if __name__ == "__main__":
+    main()
